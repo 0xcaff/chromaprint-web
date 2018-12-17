2443531b6233df0ea5ebf7630252fa00e4497dea
Martin Charles <martincharles07@gmail.com>
2018-12-17 17:41:23 -0500

    Added React

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import React from "react";
 import { ChromaprintContext } from "chromaprint-wasm";
 
 const context = new ChromaprintContext();
 context.feed(new Int16Array([1, 2, 3]));
 console.log(context.finish());
+
+export class App extends React.Component {
+  render() {
+    return <div>Hello World!!</div>;
+  }
+}
